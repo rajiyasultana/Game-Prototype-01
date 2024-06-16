293e90dc9a4a2c2bacb9c2b81f39d3bc3acec707
Rajiya Sultana <rssumu04@gmail.com>
2024-06-17 00:00:48 +0600

    Update PlayerController.cs

--- kitchenChaos/Assets/Scripts/PlayerController.cs
+++ kitchenChaos/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
-    }
+        Vector2 inputVector = new Vector2(0, 0);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if(Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = +1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = +1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = -1;
+        }
     }
 }
